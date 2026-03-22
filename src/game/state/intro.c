bdbdf2792dcce9716c69d13da3a6c06deb8cb649
Sergio López Cantero <sewave@gmail.com>
2026-03-22 21:18:09 +0100

    Read intro from files

--- src/game/state/intro.c
+++ src/game/state/intro.c
@@ -9,6 +9,10 @@
 #define LINE_Y_HALF (GAME_INTERNAL_HEIGHT / 2)
 #define NUM_COLORS 17
 #define MAX_COLOR (NUM_COLORS - 1)
+#define INTRO_TEXT_FILE_ES "assets/txt/intro_es.txt"
+#define INTRO_TEXT_FILE_EN "assets/txt/intro_en.txt"
+#define TEXTOUT_EX_BOX_BUFFER 2048
+static char buffer[TEXTOUT_EX_BOX_BUFFER];
 
 static uint8_t numLines;
 static char ** lines;
@@ -34,14 +38,60 @@ static void free_lines() {
 	linesColor = NULL;
 }
 
-void game_state_intro_init(GameContext *context) {
-	// TODO load lines from file
-	numLines = 1;
-	lines = (char **) calloc(numLines, sizeof(char *));
-	lines[0] = strdup("Lorem ipsum dolor sit amet, consectetur");
-	linesY = (int *) calloc(numLines, sizeof(int *));
-	linesColor = (int *) calloc(numLines, sizeof(int *));
+static uint8_t get_number_of_lines(const char *filename) {
+	FILE *file = fopen(filename, "r");
+	if (file == NULL) {
+		fprintf(stderr, "Error reading intro text size from [%s].\n", filename);
+		fclose(file);
+		exit(PROGRAM_ERROR);
+	}
+	uint16_t currentTextId = 0;
+	while (fgets(buffer, sizeof(buffer), file) != NULL) currentTextId++;
+	fclose(file);
+	return currentTextId;
+}
 
+static char** load_text_lines(const char *filename) {
+	char ** lines = (char **) calloc(numLines, sizeof(char *));
+	FILE *file = fopen(filename, "r");
+	if (file == NULL) {
+		fprintf(stderr, "Error reading intro text from [%s].\n", filename);
+		fclose(file);
+		exit(PROGRAM_ERROR);
+	}
+	for(int i = 0; i < numLines; i++) {
+		if(fgets(buffer, sizeof(buffer), file) == NULL) {
+			fprintf(stderr, "Error reading intro text lines from [%s].\n", filename);
+			fclose(file);
+			exit(PROGRAM_ERROR);
+		}
+		// Remove line break
+		size_t len = strlen(buffer);
+		if (len > 0 && buffer[len - 1] == '\n') buffer[len - 1] = '\0';
+		lines[i] = strdup(buffer);
+	}
+	fclose(file);
+	return lines;
+}
+
+static void load_lines_from_file(const char *filename) {
+	numLines = get_number_of_lines(filename);
+	lines = load_text_lines(filename);
+}
+
+void game_state_intro_init(GameContext *context) {
+	switch (context->config.language) {
+		case LANGUAGE_SPANISH:
+			load_lines_from_file(INTRO_TEXT_FILE_ES);
+			break;
+		case LANGUAGE_ENGLISH:
+		default:
+			load_lines_from_file(INTRO_TEXT_FILE_EN);
+			break;
+	}
+
+	linesColor = (int *) calloc(numLines, sizeof(int *));
+	linesY = (int *) calloc(numLines, sizeof(int *));
 	int lineY = LINE_Y_START + LINE_Y_SPACING;
 	for(int i = 0; i < numLines; i++) {
 		linesY[i] = lineY;
