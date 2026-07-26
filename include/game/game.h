84f98837541d2e1d67102335c448dee7ce3861db
Sergio López Cantero <sewave@gmail.com>
2026-07-26 21:09:12 +0200

    Added FPU to requirements

--- include/game/game.h
+++ include/game/game.h
@@ -372,7 +372,7 @@ void game_execute_state_render(GameContext *context, RenderQueue *renderQueue);
 // First MB is special so we check for 7 more
 #define PROGRAM_REQUIRED_RAM_MB 7
 #define MINIMAL_CPU_FAMILY CPU_FAMILY_I486
-#define REQUIRED_CPU_CAPABILITIES 0
+#define REQUIRED_CPU_CAPABILITIES CPU_FPU
 #define UNSUPPORTED_CPU_MESSAGE "Error: CPU not supported. A 486 or better with FPU is required."
 #define MINIMAP_X_POS 4
 #define MINIMAP_Y_POS 12
